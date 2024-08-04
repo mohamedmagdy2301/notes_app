d774d09980c88650cd2c97411568bdeeefc63d0a
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-05 02:03:33 +0300

    create add note state

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -4,3 +4,13 @@ part of 'add_note_cubit.dart';
 sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
+
+final class AddNoteLoading extends AddNoteState {}
+
+final class AddNoteSuccess extends AddNoteState {}
+
+final class AddNoteError extends AddNoteState {
+  final String message;
+
+  AddNoteError(this.message);
+}
