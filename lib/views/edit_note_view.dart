21c41948232185020c7a02dca22df7d9737c1da3
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-05 00:30:42 +0300

    create edit note view

---
+++ lib/views/edit_note_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/customer_appbar.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Edit Note View'),
+      ),
+    );
+  }
+}
