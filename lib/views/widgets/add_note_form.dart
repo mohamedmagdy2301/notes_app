7c497a748b351e4e10be060d567b10aa75cc469d
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-06 04:48:53 +0300

    add format for date

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/customer_elevated_button_in_bottom_sheet.dart';
@@ -60,10 +61,11 @@ class _AddNoteFormState extends State<AddNoteForm> {
   void addNoteModel(BuildContext context) {
     if (formKey.currentState!.validate()) {
       formKey.currentState!.save();
+      var formattedDate = DateFormat.yMMMd().format(DateTime.now());
       var noteModel = NoteModel(
         title: title!,
         subTitle: subTitle!,
-        date: DateTime.now().toString(),
+        date: formattedDate,
         color: Colors.yellow.value,
       );
       BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
